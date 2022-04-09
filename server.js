45ca4a9adf310156833303de8dbe60fd0d66600a
quang060697 <82989140+quang060697@users.noreply.github.com>
2022-04-10 04:58:37 +0700

    Update server.js
    Add Delete, update get functions

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ const saltRounds=10
 //app
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(express.json());
 //connect mongoDB:mongodb+srv://RedBack:<password>@cluster-redback.pa0yu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 mongoose.connect("mongodb+srv://RedBack:<password>@cluster-redback.pa0yu.mongodb.net/userDatabase?retryWrites=true&w=majority", {useNewUrlParser: true})
 
 //send value to MongoDB
+
 app.post('/user',(req,res)=>{
     const user=new User({
         username:req.body.username,
@@ -46,7 +47,41 @@ if (res.statusCode === 200)
         }
 
 })
-
+app.get( '/user',(req, res)=>{
+    User.find((err, list)=>{
+            if (err) {res.send(err)}
+            else {res.send(list)}
+        })
+    })
+app.get('/user/:id', (req, res) => {
+    User.findOne({_id: req.params.id}, (err, list)=>{
+        if (list) (res.send(list))
+        else res.send("Cannot find user id")
+    })
+})
+app.delete('/user/:id',  (req, res) => {
+    User.deleteOne({_id: req.params.id}, function(err, list) {
+        if (err) {
+          res.send("Cannot find user id");
+        } else {
+          res.send(list);
+        }
+      });
+})
+app.patch('/user/:id',(req, res)=>{
+    User.update(
+        {_id: req.params.id},
+        {$set:{username: req.body.username,
+            password: req.body.password,
+            telephone : req.body.telephone,
+            email: req.body.email,
+            }},
+        (err)=>{
+            if (!err) {res.send('Successfully update a user')}
+            else res.send(err)
+        }
+    )
+})
 //listen to 8080 port
 app.listen(8080,function(req,res){
     console.log("Web server is running in 8080...");
